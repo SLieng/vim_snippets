f4da0e17c94cc801be857be0d72d779401a56f1f
SLieng <lieng.simon96@gmail.com>
2018-06-06 02:59:44 +0100

    M ../../../autoload/snp.vim M  vap/vap_ᗰ͈.py  M ../../../tst/unt/tst_parser.py

--- rplugin/python3/snp/vap/vap_ᗰ͈.py
+++ rplugin/python3/snp/vap/vap_ᗰ͈.py
@@ -1,9 +1,5 @@
-from snp.src.parser import Parser
-
 def SNP_init(𝚵, ᖚ):
-    𝚵.Ρ̲ = {}
-    𝚵.parser = Parser()
-    𝚵.Ρ̲.update(𝚵.parser.parse('asdf'))
+    𝚵.loadSnp̲()
 
 def SNP_expand(𝚵, ᖚ):
     return 'one'
